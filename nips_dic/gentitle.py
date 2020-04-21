2a41438e9f29b0b12282229790d73efaf8a5e915
shendezhou <shendezhou@unigroup-ai.com>
2020-04-21 21:44:04 +0800

    1. add top 10 keywords papers.

--- nips_dic/gentitle.py
+++ nips_dic/gentitle.py
@@ -12,22 +12,42 @@ topten = ['andrew zisserman',
 'corinna cortes',
 'classification with']
 
-with codecs.open('papertitle.utf8', 'w', encoding='utf-8') as fw:
-    for root, dirs, files in os.walk('../acdemic_corpus/'):
-        for file in files:
-            print(file)
-            with codecs.open(root + "/" + file, 'r', encoding='utf-8') as fd:
-                base = os.path.basename(file).split(".")[0]
-                xlines = fd.readlines()
-                assert len(xlines)>0
-                xlines = [line.strip().lower() for line in xlines]
-                print(xlines)
-                for line in xlines:
-                    for bigram in topten:
-                        # words = bigram.split(' ')
-                        # if words[0] in line and words[1] in line:
-                        if bigram in line:
-                            fw.write(base+'\t'+line+'\n')
+MODE=2
+
+if MODE==1:
+    with codecs.open('papertitle%s.utf8' %topten[0], 'w', encoding='utf-8') as fw:
+        for root, dirs, files in os.walk('../acdemic_corpus/'):
+            for file in files:
+                print(file)
+                with codecs.open(root + "/" + file, 'r', encoding='utf-8') as fd:
+                    base = os.path.basename(file).split(".")[0]
+                    xlines = fd.readlines()
+                    assert len(xlines)>0
+                    xlines = [line.strip().lower() for line in xlines]
+                    print(xlines)
+                    for line in xlines:
+                        for bigram in topten:
+                            # words = bigram.split(' ')
+                            # if words[0] in line and words[1] in line:
+                            if bigram in line:
+                                fw.write(base+'\t'+line+'\n')
+if MODE==2:
+    for bigram in topten:
+        with codecs.open('papertitle%s.utf8' % bigram.replace(" ",""), 'w', encoding='utf-8') as fw:
+            for root, dirs, files in os.walk('../acdemic_corpus/'):
+                for file in files:
+                    print(file)
+                    with codecs.open(root + "/" + file, 'r', encoding='utf-8') as fd:
+                        base = os.path.basename(file).split(".")[0]
+                        xlines = fd.readlines()
+                        assert len(xlines)>0
+                        xlines = [line.strip().lower() for line in xlines]
+                        print(xlines)
+                        for line in xlines:
+                            # words = bigram.split(' ')
+                            # if words[0] in line and words[1] in line:
+                            if bigram in line:
+                                fw.write(base+'\t'+line+'\n')
 print('FIN')
 
 
